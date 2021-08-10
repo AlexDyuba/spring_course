d4b44d96682effd1e6252937502ad6012cabd8a3
AlexDyuba <adyuba@icloud.com>
2021-08-10 16:12:17 +0300

    DI with construct

--- src/main/java/com/alexDyuba/spring/TestPet.java
+++ src/main/java/com/alexDyuba/spring/TestPet.java
@@ -6,9 +6,8 @@ public class TestPet {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
-
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        context.close();
     }
 }
