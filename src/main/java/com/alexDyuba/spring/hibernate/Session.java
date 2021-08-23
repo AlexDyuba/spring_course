b27bb582008e97e29cc377d7d7bd9666bfc5383e
AlexDyuba <adyuba@icloud.com>
2021-08-23 19:46:00 +0300

    Session

---
+++ src/main/java/com/alexDyuba/spring/hibernate/Session.java
@@ -0,0 +1,22 @@
+package com.alexDyuba.spring.hibernate;
+import com.alexDyuba.*;
+import com.alexDyuba.spring.hibernate.entity.Employee;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Session {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        org.hibernate.Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = new Employee("Alex", "Dyuba", "IT", 2500);
+
+        session.beginTransaction();
+        session.save(employee);
+        session.getTransaction().commit();
+    }
+}
