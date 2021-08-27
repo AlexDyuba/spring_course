5e682e8ba00d068787bc62690c9b65f37a20eec6
AlexDyuba <adyuba@icloud.com>
2021-08-27 13:49:45 +0300

    createQuare

--- src/main/java/com/alexDyuba/spring/hibernate/Session.java
+++ src/main/java/com/alexDyuba/spring/hibernate/Session.java
@@ -3,6 +3,8 @@ import com.alexDyuba.spring.hibernate.entity.Employee;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Session {
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = new Configuration()
@@ -10,10 +12,16 @@ public class Session {
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory()) {
             org.hibernate.Session session = sessionFactory.getCurrentSession();
-            Employee employee = new Employee("Glev", "Smirny", "HR", 1825);
             session.beginTransaction();
-            session.save(employee);
+            List<Employee> employeeList = session.createQuery("from Employee")
+                    .getResultList();
+
+            for (Employee i : employeeList) {
+                System.out.println(i);
+            }
+
             session.getTransaction().commit();
+            sessionFactory.close();
         }
     }
 }
